88924fef3dad4e531ae26fa6d15a12a0f55fcfe4
John Botero <jhondark@gmail.com>
2020-08-28 01:55:08 -0500

    update render and styles

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { render } from "react-dom"
 
 class CardApp extends React.Component {
     constructor(props) {
@@ -6,13 +7,13 @@ class CardApp extends React.Component {
         this.state = { cards: [] }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         fetch("https://api.magicthegathering.io/v1/cards")
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
-                this.setState({ cards: data })
+                this.setState({ cards: data.cards })
                 console.log(this.state.cards)
             })
     }
@@ -22,7 +23,14 @@ class CardApp extends React.Component {
             <>
                 {this.state.cards.length > 0 ? (
                     this.state.cards.map((card, i) => {
-                        return <h3 key={i}>{card.name}</h3>
+                        return (
+                            <div className="container__card">
+                                <div className="container__cards--item">
+                                    <h4>{card.name}</h4>
+                                    <img src={card.imageUrl} alt="" />
+                                </div>
+                            </div>
+                        )
                     })
                 ) : (
                     <p>Loading...</p>
@@ -33,3 +41,48 @@ class CardApp extends React.Component {
 }
 
 export default CardApp
+
+//------------HOOKS -----------------------
+/*
+function useCards() {
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [cards, setCards] = useState({ cards: [] })
+
+    useEffect(() => {
+        fetch("https://api.magicthegathering.io/v1/cards")
+            .then((res) => res.json())
+            .then(
+                (data) => {
+                    setCards({ cards: data.cards })
+                    console.log(cards)
+                },
+                (error) => {
+                    setIsLoaded(true)
+                    setError(error)
+                }
+            )
+    })
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+        return <div className="loading">Loading...</div>
+    } else {
+        return (
+            <div className="container__card">
+                <div className="container__cards--item">
+                    <h4>{cards.name}</h4>
+                    <img src={cards.imageUrl} alt="card" />
+                </div>
+            </div>
+        )
+    }
+}
+
+const CardsConter = () => {
+    return <>{useCards()}</>
+}
+
+export default CardsConter
+*/
